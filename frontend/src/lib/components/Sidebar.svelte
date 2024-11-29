1239c20e93fe52417412c73952c002751a1b936b
02danieljonas <02danieljonas@gmail.com>
2024-11-29 16:34:32 -0500

    added sticky

--- frontend/src/lib/components/Sidebar.svelte
+++ frontend/src/lib/components/Sidebar.svelte
@@ -1,11 +1,13 @@
 <script>
-    const onclick = ()=>{
-        alert("Complete function")
-    }
+    const onclick = () => {
+        alert("Complete function");
+    };
 </script>
 
 <div class="w-full bg-[#d9d9d9] h-full">
-    <button class="block" {onclick}>Create a new password</button>
-    <button class="block" {onclick}>Two Factor Authetication</button>
-    <button class="block" {onclick}>Edit Password</button>
+    <div class="sticky top-0">
+        <button class="block" {onclick}>Create a new password</button>
+        <button class="block" {onclick}>Two Factor Authetication</button>
+        <button class="block" {onclick}>Edit Password</button>
+    </div>
 </div>
