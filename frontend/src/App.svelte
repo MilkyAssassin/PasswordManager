89ee46075290de3ab180d66123a68d8573f4f020
02danieljonas <02danieljonas@gmail.com>
2024-11-29 11:20:51 -0500

    added tailwind

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,18 +2,19 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  
 </script>
 
 <main>
   <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
+    <a href="https://vite.dev" target="_blank" rel="noreferrer" >
       <img src={viteLogo} class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1 class="text-3xl font-bold underline bg-black text-green-950">Vite + Svelte</h1>
 
   <div class="card">
     <Counter />
