36870245bd961a8f51f0a8072c2688f6d6ff3b51
02danieljonas <02danieljonas@gmail.com>
2024-11-29 12:27:10 -0500

    prototype of sidebar

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,48 +1,13 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  
+    import Vault from "./lib/components/Vault/index.svelte";
+    import Sidebar from "./lib/components/Sidebar.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer" >
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1 class="text-3xl font-bold underline bg-black text-green-950">Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<main class="h-dvh grid grid-cols-5">
+    <div class="">
+        <Sidebar />
+    </div>
+    <div class="col-span-4">
+        <Vault />
+    </div>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
---
+++ frontend/src/lib/components/Vault/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    
+</script>
+
+<div class="w-full bg-gray-600 h-full">
+    <button class="block">Create a new password</button>
+    <button class="block">Two Factor Authetication</button>
+    <button class="block">Edit Password</button>
+</div>
