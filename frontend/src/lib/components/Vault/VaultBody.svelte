4e332657f175b67619653eca825fbe391ee63c82
02danieljonas <02danieljonas@gmail.com>
2024-11-29 15:39:54 -0500

    rendered information for vault

--- frontend/src/lib/components/Vault/VaultBody.svelte
+++ frontend/src/lib/components/Vault/VaultBody.svelte
@@ -2,25 +2,35 @@
     import { getContext } from "svelte";
     import type { Context } from "../../../types/types";
 
-    
-    type sortType = "AlphabeticalOrder"
-    | "AlphabeticalOrderReverse"
-    | "DateCreated"
-            | "DateCreatedReverse";
+    type sortType =
+        | "AlphabeticalOrder"
+        | "AlphabeticalOrderReverse"
+        | "DateCreated"
+        | "DateCreatedReverse";
     type props = {
         search: string;
-        sort: sortType
+        sort: sortType;
     };
-    
-    let { search = $bindable(), sort = $bindable() }: props = $props();
-    let context:Context = getContext("context");
 
+    let { search = $bindable(), sort = $bindable() }: props = $props();
+    let context: Context = getContext("context");
+    let vault = $derived(context.user?.vault||[]);
 </script>
 
-
 <div>
-    Search: {search}
     {#if context.isLoading}
         Loading...
+    {:else if 0 < vault.length}
+        {#each vault as entry}
+            <div class="flex justify-between px-10 py-4">
+                <div class="flex gap-4">
+                    <div class="rounded-full bg-[#d8cbea] text-[#625282] h-7 w-7 text-center align-middle leading-7">{entry.username[0]}</div>
+                    {entry.title}
+                </div>
+                <span>|</span>
+            </div>
+        {/each}
+    {:else}
+        Your vault is empty, please add a password
     {/if}
 </div>
