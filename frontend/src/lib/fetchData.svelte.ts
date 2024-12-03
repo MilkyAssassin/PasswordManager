980956d91d59875049dc2ec53fa6a29765e58676
02danieljonas <02danieljonas@gmail.com>
2024-12-03 15:01:13 -0500

    added some methods

--- frontend/src/lib/fetchData.svelte.ts
+++ frontend/src/lib/fetchData.svelte.ts
@@ -1,7 +1,69 @@
 import { faker } from "@faker-js/faker";
 import type { User, Entry } from "@my-types/types";
-
 const API_URL = "";
+const LOGIN_ENDPOINT = API_URL + "";
+const LOGIN_METHOD = "";
+const REGISTER_ENDPOINT = API_URL + "";
+const REGISTER_METHOD = "";
+const DELETE_ENDPOINT = API_URL + "";
+const DELETE_METHOD = "";
+const EDIT_ENDPOINT = API_URL + "";
+const EDIT_METHOD = "";
+const ADD_ENDPOINT = API_URL + "";
+const ADD_METHOD = "";
+
+export async function login(data: {
+    username: string;
+    password: string;
+}): Promise<boolean> {
+    try {
+        const res = await fetch(LOGIN_ENDPOINT, {
+            method: LOGIN_METHOD,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        if (res.ok) {
+            const data = await res.json();
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+
+            return true;
+        }
+    } finally {
+        return false;
+    }
+}
+
+export async function register(data: { username: string; password: string }) {
+    try {
+        const res = await fetch(REGISTER_ENDPOINT, {
+            method: REGISTER_METHOD,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        if (res.ok) {
+            const data = await res.json();
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            return true;
+        }
+    } finally {
+        return false;
+    }
+}
+
 export async function fetchUser(): Promise<User> {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -9,6 +71,79 @@ export async function fetchUser(): Promise<User> {
         }, 1000);
     });
 }
+
+export async function deletePassword(data: { passwordId: number }) {
+    try {
+        const res = await fetch(DELETE_ENDPOINT, {
+            method: DELETE_METHOD,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        if (res.ok) {
+            const data = await res.json();
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            return true;
+        }
+    } finally {
+        return false;
+    }
+}
+
+export async function addPassword(data:Entry) {
+    try {
+        const res = await fetch(ADD_ENDPOINT, {
+            method: ADD_METHOD,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        if (res.ok) {
+            const data = await res.json();
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            return true;
+        }
+    } finally {
+        return false;
+    }
+
+}
+
+export async function editPassword(data:Entry) {
+    try {
+        const res = await fetch(EDIT_ENDPOINT, {
+            method: EDIT_METHOD,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        if (res.ok) {
+            const data = await res.json();
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            console.log("Get cookie from fetch");
+            console.error("Get cookie from fetch");
+            return true;
+        }
+    } finally {
+        return false;
+    }}
+
 function fakeFetchUser(): User {
     let response: User = {
         username: faker.internet.username(),
@@ -17,7 +152,7 @@ function fakeFetchUser(): User {
     const num_of_entries = Math.floor(Math.random() * (25 - 0 + 1)) + 0;
 
     for (let i = 1; i <= num_of_entries; i++) {
-        console.log(faker.person.fullName())
+        console.log(faker.person.fullName());
         const entry: Entry = {
             id: i,
             title: faker.company.name(),
@@ -25,10 +160,14 @@ function fakeFetchUser(): User {
             url: faker.internet.url(),
             password: faker.internet.password(),
             notes: faker.lorem.text(),
-            dateCreated: faker.date
-                .between({ from: "2000-01-01", to: "2015-01-01" }),
-            lastUsed: faker.date
-                .between({ from: "2020-01-01", to: Date.now() }),
+            dateCreated: faker.date.between({
+                from: "2000-01-01",
+                to: "2015-01-01",
+            }),
+            lastUsed: faker.date.between({
+                from: "2020-01-01",
+                to: Date.now(),
+            }),
         };
         response.vault.push(entry);
     }
