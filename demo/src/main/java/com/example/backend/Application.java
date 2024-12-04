4aa300925d9553f9d75d96e066f78925aba7807c
Alejandro Cancio <acancio92004@gmail.com>
2024-12-04 12:41:10 -0500

    Working Endpoints

--- demo/src/main/java/com/example/backend/Application.java
+++ demo/src/main/java/com/example/backend/Application.java
@@ -1,12 +1,14 @@
 
-package main.java.com.example.backend;  // Remove main.java prefix
-
+package main.java.com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "main.java.com.example.backend", exclude = {
-    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
-    )
+@ComponentScan("main.java.com.example.backend.controller")
+@SpringBootApplication(
+    scanBasePackages = "main.java.com.example.backend.controller",
+    exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
+)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- demo/src/main/java/com/example/backend/controller/Test.java
+++ demo/src/main/java/com/example/backend/controller/Test.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
 @RestController
-public class TestController {
+public class Test {
 
     @GetMapping("/test")
     public ResponseEntity<String> testEndpoint() {
